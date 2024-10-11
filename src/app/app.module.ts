a11f63cb5c0b50a1fc9a5993f593d827dcedbdde
Zahra Goudarzi <zahra.goudarzi@stud.uni-bamberg.de>
2024-10-11 17:56:31 +0200

    Not Important Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { CommonModule, NgFor } from '@angular/common';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { FormsModule } from '@angular/forms';
-import { CardComponent } from './shared/card/card.component';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
+  declarations: [AppComponent, UserComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
     HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
-    CardComponent,
+    SharedModule,
+    TasksModule,
   ],
-  imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
